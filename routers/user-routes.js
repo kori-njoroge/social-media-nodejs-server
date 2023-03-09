cd830fd997f3be4499450f5a71d9aaf0d5ae48f4
skeizable <kibetrns@protonmail.com>
2023-03-09 04:50:40 +0300

    add route for profile editing

--- routers/user-routes.js
+++ routers/user-routes.js
@@ -5,7 +5,8 @@ const { SchemaValidateMiddleware } = require('../middlewares/schema-validate');
 const {signUpSchema, loginSchema} = require('../services/joi-services');
 const {
     addUser,
-    userLogin
+    userLogin,
+    editProfile
 } = require('../controllers/user-controllers');
 
 
@@ -13,6 +14,7 @@ const {
 userRouter.post('/signup',(req,res,next) =>SchemaValidateMiddleware(req,res,next,signUpSchema), addUser);
 userRouter.post('/login',(req,res,next) =>SchemaValidateMiddleware(req,res,next,loginSchema),userLogin);
 
+userRouter.patch('/:userId/edit-profile', editProfile);
 
 
 
