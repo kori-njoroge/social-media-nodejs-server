ffe0d08dc62253d93b8cef0c4e5b71965e917524
kibetrns <70756575+kibetrns@users.noreply.github.com>
2023-03-09 13:29:14 +0300

    Revert "HotFix login and sign up controllers"

--- services/joi-services.js
+++ services/joi-services.js
@@ -9,7 +9,7 @@ const Joi = require('joi');
 // is_deleted BIT DEFAULT 0
 
 const signUpSchema = Joi.object({
-    fullName: Joi.string()
+    full_name: Joi.string()
         .required(),
 
     userName: Joi.string()
@@ -19,27 +19,26 @@ const signUpSchema = Joi.object({
         .email({ minDomainSegments: 2, tlds: ['*'] })
         .required(),
 
-    phoneNumber: Joi.number()
+    phonenuber: Joi.number()
         .min(9)
+        .max(12)
         .required(),
 
-    country: Joi.string()
-        .required(),
+    country: Joi.string(),
 
 
-    gender: Joi.string()
-        .required(),
+    gender:Joi.string(),
 
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z1-9]{5,30}'))
         .required(),
 
-    confirmPassword: Joi.ref('password'),
-}).with('password', 'confirmPassword')
+    connfirmpassword: Joi.ref('password'),
+}).with('password', 'confirmpassword')
 
 const loginSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
 });
 
-module.exports = { signUpSchema, loginSchema }
+module.exports = {signUpSchema, loginSchema}
